6538f9c6b7f0b7346921a3612aa1fc8f436430ee
s96603ainTSAmir <MasriBoje@gmail.com>
2025-09-23 00:20:30 +0100

    调整了 settings.json 配置文件

---
+++ docs/rspress.config.ts
@@ -0,0 +1,22 @@
+import * as path from 'node:path';
+import { defineConfig } from 'rspress/config';
+
+export default defineConfig({
+  root: path.join(__dirname, 'docs'),
+  base: '/cpp-console-apps/', // For hosting to https://example.com/cpp-console-apps/
+  title: 'C/C++ Console Apps',
+  icon: '/rspress-icon.png',
+  logo: {
+    light: '/rspress-light-logo.png',
+    dark: '/rspress-dark-logo.png',
+  },
+  themeConfig: {
+    socialLinks: [
+      {
+        icon: 'github',
+        mode: 'link',
+        content: 'https://github.com/junian/cpp-console-apps/',
+      },
+    ],
+  },
+});
